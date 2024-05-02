34d080756079e41d757b3a131ccbdbcc60e96d9b
alasekri <milankooora@gmail.com>
2024-05-02 23:25:19 +0200

    hash

--- app/models/userModle.ts
+++ app/models/userModle.ts
@@ -1,28 +1,40 @@
+import { genSalt, hash } from "bcrypt";
 import { UUID, randomUUID } from "crypto";
 import { Document, Model, Schema, model, models } from "mongoose";
 
-interface UserDocument extends Document{
-    name: string;
-    email: string;
-    password: string;
-    role: "admin" | "user";
-    avatar: {url: string; id:UUID};
+interface UserDocument extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: "admin" | "user";
+  avatar: { url: string; id: UUID };
 
-    verified: boolean;
-
+  verified: boolean;
 }
 
-const userSchema = new Schema<UserDocument>({
-    name: {type:String, required:true,trim: true },
-    email: {type:String, required:true , unique: true},
-    password: {type:String, required:true},
-    role: {type:String, enum:['admin' , 'user'] , default:'user'},
-    avatar: {type:Object, url: String , id:randomUUID},
-    verified: {type:Boolean, default:false},
+const userSchema = new Schema<UserDocument>(
+  {
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["admin", "user"], default: "user" },
+    avatar: { type: Object, url: String, id: randomUUID },
+    verified: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
-},{timestamps: true});
+userSchema.pre("save", async function (next) {
+  try {
+    if (!this.isModified("password")) return next();
+    const salt = await genSalt(10);
+    this.password = await hash(this.password, salt);
+    next();
+  } catch (error) {
+    throw error;
+  }
+});
 
-const UserModel = models.User || model('User', userSchema)
+const UserModel = models.User || model("User", userSchema);
 
-
-export default UserModel as Model<UserDocument>
+export default UserModel as Model<UserDocument>;
